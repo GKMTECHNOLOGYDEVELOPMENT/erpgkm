025df796de0c543428f286771ceaedf852d593c9
GKMTECHNOLOGYDEVELOPMENT <softwaredevelopmentgkm@gkmtechnology.com.pe>
2025-03-03 08:36:11 -0500

    add

--- resources/views/tickets/ordenes-trabajo/smart-tv/detalle/index.blade.php
+++ resources/views/tickets/ordenes-trabajo/smart-tv/detalle/index.blade.php
@@ -1,7 +1,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script> -->
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <!-- Flatpickr CSS -->
