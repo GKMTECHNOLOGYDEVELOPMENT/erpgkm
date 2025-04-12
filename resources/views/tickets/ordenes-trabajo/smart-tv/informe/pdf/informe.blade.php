6b84548ecf702b6d1d2ad11a9931f495adc0e607
Stuard Rafael Huarcaya Granados <72283677+Rafael-PG@users.noreply.github.com>
2025-04-12 11:01:27 -0500

    add

--- resources/views/tickets/ordenes-trabajo/smart-tv/informe/pdf/informe.blade.php
+++ resources/views/tickets/ordenes-trabajo/smart-tv/informe/pdf/informe.blade.php
@@ -320,7 +320,7 @@
                     @if ($firmaCliente)
                         <img src="{{ $firmaCliente }}" alt="Firma del Cliente"
                             class="w-[90%] h-20 mx-auto object-contain absolute"
-                            style="transform: scale(1.5); transform-origin: bottom center; bottom: -20px; position: relative;">
+                            style="transform: scale(1.5); transform-origin: bottom center; bottom: -30px; position: relative;">
                     @else
                         <div class="h-full flex items-center justify-center w-full">
                             <p class="text-xs text-gray-500 font-bold">Cliente no firmó</p>
