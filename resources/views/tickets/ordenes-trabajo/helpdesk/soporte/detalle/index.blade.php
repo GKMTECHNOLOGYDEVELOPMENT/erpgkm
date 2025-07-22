398832c315ef23a20fc56e06a88560ddfc7c7744
GKMTECHNOLOGYDEV31 <softwaredevelopmentgkm2@gkmtechnology.com.pe>
2025-07-22 14:57:48 -0500

    a

--- resources/views/tickets/ordenes-trabajo/helpdesk/soporte/detalle/index.blade.php
+++ resources/views/tickets/ordenes-trabajo/helpdesk/soporte/detalle/index.blade.php
@@ -1104,8 +1104,6 @@
                 }
             }
 
-
-
             // Obtener el token CSRF desde la página
             var csrfToken = $('meta[name="csrf-token"]').attr('content');
             console.log("Token CSRF obtenido:",
